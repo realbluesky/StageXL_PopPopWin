1c302dedb92d045fde1f73d31628994e0caa39a8
Kevin Moore <github@j832.com>
2014-04-06 10:00:10 -0700

    Removed unused code

--- lib/src/html.dart
+++ lib/src/html.dart
@@ -1,15 +1,12 @@
 library pop_pop_win.html;
 
 import 'dart:async';
-import 'dart:html';
 
 import 'package:bot/bot.dart';
 import 'package:pop_pop_win/platform_target.dart';
 
 import 'game.dart';
 
-part 'html/high_score_view.dart';
-part 'html/game_view.dart';
 part 'html/game_storage.dart';
 part 'html/game_manager.dart';
 
