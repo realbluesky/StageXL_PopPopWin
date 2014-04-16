7dd72d5bd1e286adf35a2ea83d93bb3b47ebef86
Kevin Moore <github@j832.com>
2014-04-16 15:18:52 -0700

    a bit more cleanup

--- lib/pop_pop_win.dart
+++ lib/pop_pop_win.dart
@@ -1,5 +1,6 @@
 library pop_pop_win;
 
+import 'dart:async';
 import 'dart:html';
 import 'dart:math' as math;
 
@@ -17,87 +18,89 @@ const String _TRANSPARENT_TEXTURE = '${_ASSET_DIR}images/transparent.json';
 const String _OPAQUE_TEXTURE = '${_ASSET_DIR}images/opaque.json';
 const String _TRANSPARENT_STATIC_TEXTURE = '${_ASSET_DIR}images/static.json';
 
-void startGame(PlatformTarget platform) {
+Future startGame(PlatformTarget platform) {
   initPlatform(platform);
 
-  Stage stage = new Stage(querySelector('#gameCanvas'), webGL: true, color:
-      0xb4ad7f, frameRate: 60);
-  RenderLoop renderLoop = new RenderLoop();
-  renderLoop.addStage(stage);
+  var stage = new Stage(querySelector('#gameCanvas'), webGL: true,
+      color: 0xb4ad7f, frameRate: 60);
+
+  var renderLoop = new RenderLoop()
+      ..addStage(stage);
 
   BitmapData.defaultLoadOptions.webp = true;
 
   //have to load the loading bar first...
-  var resourceManager = new ResourceManager()..addTextureAtlas("static",
-      "resources/images/static.json", TextureAtlasFormat.JSON);
+  var resourceManager = new ResourceManager()
+      ..addTextureAtlas("static", "resources/images/static.json",
+          TextureAtlasFormat.JSON);
 
-  resourceManager.load().then((r) {
+  return resourceManager.load()
+      .then((resMan) => _initialLoad(resMan, stage));
+}
 
-    TextureAtlas ta = resourceManager.getTextureAtlas('static');
+void _initialLoad(ResourceManager resourceManager, Stage stage) {
+  var atlas = resourceManager.getTextureAtlas('static');
 
-    var bar = new Gauge(ta.getBitmapData('loading_bar'), Gauge.DIRECTION_RIGHT)
-        ..x = 51
-        ..y = 8
-        ..ratio = 0;
+  var bar = new Gauge(atlas.getBitmapData('loading_bar'), Gauge.DIRECTION_RIGHT)
+      ..x = 51
+      ..y = 8
+      ..ratio = 0;
 
-    var loadingText = new Bitmap(ta.getBitmapData('loading_text'))
-        ..x = 141
-        ..y = 10;
+  var loadingText = new Bitmap(atlas.getBitmapData('loading_text'))
+      ..x = 141
+      ..y = 10;
 
-    var loadingSprite = new Sprite()
-        ..addChild(new Bitmap(ta.getBitmapData('loading_background')))
-        ..addChild(bar)
-        ..addChild(loadingText)
-        ..x = stage.sourceWidth ~/ 2 - 1008 ~/ 2
-        ..y = 400
-        ..scaleX = 2
-        ..scaleY = 2
-        ..addTo(stage);
+  var loadingSprite = new Sprite()
+      ..addChild(new Bitmap(atlas.getBitmapData('loading_background')))
+      ..addChild(bar)
+      ..addChild(loadingText)
+      ..x = stage.sourceWidth ~/ 2 - 1008 ~/ 2
+      ..y = 400
+      ..scaleX = 2
+      ..scaleY = 2
+      ..addTo(stage);
 
-    resourceManager
-        ..addTextureAtlas('opaque', 'resources/images/opaque.json',
-            TextureAtlasFormat.JSON)
-        ..addTextureAtlas('animated', 'resources/images/animated.json',
-            TextureAtlasFormat.JSON);
+  resourceManager
+      ..addTextureAtlas('opaque', 'resources/images/opaque.json',
+          TextureAtlasFormat.JSON)
+      ..addTextureAtlas('animated', 'resources/images/animated.json',
+          TextureAtlasFormat.JSON);
 
-    resourceManager.addSoundSprite('audio', 'resources/audio/audio.json');
+  resourceManager.addSoundSprite('audio', 'resources/audio/audio.json');
 
-    resourceManager.onProgress.listen((e) {
-      bar.ratio = resourceManager.finishedResources.length /
-          resourceManager.resources.length;
-    });
-
-    resourceManager.load().then((r) {
-
-      Tween tween = stage.juggler.tween(loadingSprite, .5)..animate.alpha.to(0);
-      tween.onComplete = () => stage.removeChild(loadingSprite);
-
-      _updateAbout();
-
-      targetPlatform.aboutChanged.listen((_) => _updateAbout());
-
-      var size = targetPlatform.size;
-      var m = (size * size * 0.15625).toInt();
-
-      var _audio = new _Audio(resourceManager);
-      var gameRoot = new GameRoot(size, size, m, stage, resourceManager);
-
-      // disable touch events
-      window.onTouchMove.listen((args) => args.preventDefault());
-
-      window.onKeyDown.listen(_onKeyDown);
-
-      querySelector('#popup').onClick.listen(_onPopupClick);
-
-      titleClickedEvent.listen((args) => targetPlatform.toggleAbout(true));
-
-    }).catchError((error) {
-      for (var resource in resourceManager.failedResources) {
-        print("Loading resouce failed: "
-            "${resource.kind}.${resource.name} - ${resource.error}");
-      }
-    });
+  resourceManager.onProgress.listen((e) {
+    bar.ratio = resourceManager.finishedResources.length /
+        resourceManager.resources.length;
   });
+
+  resourceManager.load().then((resMan) =>
+      _secondaryLoad(resMan, stage, loadingSprite));
+}
+
+void _secondaryLoad(ResourceManager resourceManager, Stage stage,
+    Sprite loadingSprite) {
+  var tween = stage.juggler.tween(loadingSprite, .5)
+      ..animate.alpha.to(0)
+      ..onComplete = () => stage.removeChild(loadingSprite);
+
+  _updateAbout();
+
+  targetPlatform.aboutChanged.listen((_) => _updateAbout());
+
+  var size = targetPlatform.size;
+  var m = (size * size * 0.15625).toInt();
+
+  var _audio = new _Audio(resourceManager);
+  var gameRoot = new GameRoot(size, size, m, stage, resourceManager);
+
+  // disable touch events
+  window.onTouchMove.listen((args) => args.preventDefault());
+
+  window.onKeyDown.listen(_onKeyDown);
+
+  querySelector('#popup').onClick.listen(_onPopupClick);
+
+  titleClickedEvent.listen((args) => targetPlatform.toggleAbout(true));
 }
 
 void _onPopupClick(args) {
